1c0c358b09e495db6fbbc0ea84ae36eca06e7638
nohbdy-exe <bryantl1337@gmail.com>
2025-02-12 03:33:24 -0600

    Stable release v1, merged all branches

--- OrderMgmtRevision/Controllers/CustomersController.cs
+++ OrderMgmtRevision/Controllers/CustomersController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OrderMgmtRevision.Models;
 
 namespace OrderMgmtRevision.Controllers
 {
+    [Authorize]
     public class CustomersController : Controller
     {
         public IActionResult Index()
