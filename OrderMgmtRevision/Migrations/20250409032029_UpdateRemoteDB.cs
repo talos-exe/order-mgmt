317c8453bdb1fb2e1db2dd8ec23dc41daef55033
nohbdy-exe <bryantl1337@gmail.com>
2025-04-08 22:26:02 -0500

    quick fix

---
+++ OrderMgmtRevision/Migrations/20250409032029_UpdateRemoteDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderMgmtRevision.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRemoteDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipAmount",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ShipAmount",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
