17ea19bf2ab1d0dd1bbded97418a55aad2ebbab6
nohbdy-exe <bryantl1337@gmail.com>
2025-04-27 05:37:02 -0500

    slight bug fixes on peytons branch

--- OrderMgmtRevision/Controllers/WarehouseController.cs
+++ OrderMgmtRevision/Controllers/WarehouseController.cs
@@ -10,28 +10,6 @@ namespace OrderMgmtRevision.Controllers
     public class WarehouseController : Controller
     {
         private readonly AppDbContext _context;
-
-        public WarehouseController(AppDbContext context)
-        {
-            var warehouseData = new List<Warehouse>
-            {
-              new Warehouse 
-              {
-                WarehouseID = 123,
-                WarehouseName = "Arctic Warehouse",
-                Address = "123 Test St"
-              },
-              new Warehouse 
-              {
-                WarehouseID = 456,
-                WarehouseName = "Warehouse 2",
-                Address = "456 Test St"
-              }
-            };
-
-<<<<<<< Updated upstream
-            return View(warehouseData);
-=======
         public WarehouseController(AppDbContext context)
         {
             _context = context;
@@ -97,7 +75,6 @@ namespace OrderMgmtRevision.Controllers
 
             ViewBag.WarehouseName = (await _context.Warehouses.FindAsync(warehouseId))?.WarehouseName;
             return View(workOrders);
->>>>>>> Stashed changes
         }
     }
 }
